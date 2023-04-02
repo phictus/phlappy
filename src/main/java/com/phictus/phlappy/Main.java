ced26ffa1b5cfef22abe4a1f1dbbcf835b144eee
Parsa Jokar <prsjokar@gmail.com>
2023-04-02 19:35:18 +0430

    flap mechanics

--- src/main/java/com/phictus/phlappy/Main.java
+++ src/main/java/com/phictus/phlappy/Main.java
@@ -1,63 +1,7 @@
 package com.phictus.phlappy;
 
-import org.lwjgl.opengl.GL;
-
-import com.phictus.phlappy.entities.Bird;
-import com.phictus.phlappy.graphics.Shader;
-import com.phictus.phlappy.graphics.Texture2D;
-import com.phictus.phlappy.math.Mat4;
-
-import static org.lwjgl.system.MemoryUtil.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL33C.*;
-
-
 public class Main {
     public static void main(String[] args) {
-        glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        long window = glfwCreateWindow(1280, 720, "phlappy", NULL, NULL);
-        glfwMakeContextCurrent(window);
-        GL.createCapabilities();
-        glfwSwapInterval(1);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        Shader.init();
-        Texture2D.init();
-
-        Bird bird = new Bird();
-        bird.init();
-
-        Mat4 viewProjection = Mat4.orthographic(0.0f, 1280.0f, 0.0f, 720.0f, -1.0f, 1.0f);
-        Shader.texture.use();
-        Shader.texture.setUniformMat4("u_ViewProjection", viewProjection);
-
-        double lastFrameTime = 0.0;
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-
-            final double time = glfwGetTime();
-            final float deltaTime = (float)(time - lastFrameTime);
-            lastFrameTime = time;
-
-            bird.update(deltaTime);
-
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            bird.render();
-
-            glfwSwapBuffers(window);
-        }
-
-        bird.destroy();
-
-        Texture2D.destroy();
-        Shader.destroy();
-
-        glfwDestroyWindow(window);
-        glfwTerminate();
+        Game.run();
     }
 }
--- src/main/java/com/phictus/phlappy/entities/Bird.java
+++ src/main/java/com/phictus/phlappy/entities/Bird.java
@@ -1,5 +1,6 @@
 package com.phictus.phlappy.entities;
 
+import com.phictus.phlappy.Game;
 import com.phictus.phlappy.graphics.Shader;
 import com.phictus.phlappy.graphics.Texture2D;
 
@@ -7,10 +8,10 @@ public class Bird extends Entity {
     @Override
     protected void onStart() {
         float[] vboData = new float[] {
-            0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 0.0f, 0.0f
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+             0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
+             0.5f,  0.5f, 0.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f, 0.0f, 0.0f, 0.0f
         };
         int[] eboData = new int[] { 0, 2, 1, 0, 2, 3 };
 
@@ -19,15 +20,37 @@ public class Bird extends Entity {
         mesh.addAttrib(2);
         shader = Shader.texture;
         texture = Texture2D.bird;
-        position.x = 0.0f;
-        position.y = 0.0f;
-        position.z = 0.9f;
         scale.x = 85.0f;
         scale.y = 60.0f;
+        position.x = 1280.0f / 2.0f;
+        position.y = 720.0f / 2.0f;
+        position.z = 0.1f;
     }
 
     @Override
     protected void onUpdate(final float deltaTime) {
+        if (Game.isSpaceDown()) {
+            if (isFlapAvailable) {
+                velocity = 500.0f;
+                isFlapAvailable = false;
+            }
+        }
+        else
+            isFlapAvailable = true;
 
+        if (Game.isRunning()) {
+            rotation = velocity / 500.0f + 1.5f;
+            rotation = Math.max((float)Math.toRadians(MIN_ROTATION), Math.min((float)Math.toRadians(MAX_ROTATION), rotation));
+
+            velocity += ACCELERATION * deltaTime;
+            position.y += velocity * deltaTime;
+        }
     }
+
+    private static final float ACCELERATION = -2000.0f;
+    private static final double MAX_ROTATION = 30.0f;
+    private static final double MIN_ROTATION = -30.0f;
+
+    private boolean isFlapAvailable = true;
+    private float velocity = 0.0f;
 }
